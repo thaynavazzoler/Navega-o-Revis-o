86aeec40eb7bd5718103f8a8fefb37674a0c2005
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-11 14:25:14 -0300

    "tab routes"

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,3 +1,4 @@
+import { Feather } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Category from "../screens/Category";
 import Profile from "../screens/Profile";
@@ -6,12 +7,61 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
-return (
-  <Tab.Navigator>
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Category" component={Category} />
-    <Tab.Screen name="Profile" component={Profile} />
-  </Tab.Navigator>
-);
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#DDD",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="home"
+              size={24}
+              color={focused ? "red" : "#D6D6D6"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Category"
+        component={Category}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="list"
+              size={24}
+              color={focused ? "#131313" : "#D6D6D6"}
+            />
+          ),
+          tabBarLabel: "Categoria",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#777",
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="user"
+              size={24}
+              color={focused ? "#131313" : "#D6D6D6"}
+            />
+          ),
+          tabBarLabel: "Perfil",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#D6D6D6",
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 export default TabRoutes;
